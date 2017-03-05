7a40df3278a24770fc5e98aeb35718ad63afe21b
Michal Jonko <michal.jonko@gmail.com>
2017-03-05 23:47:25 +0100

    increased tests cover

--- src/test/java/pl/coffeepower/guiceliquibase/Example.java
+++ src/test/java/pl/coffeepower/guiceliquibase/Example.java
@@ -1,23 +1,33 @@
 package pl.coffeepower.guiceliquibase;
 
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.Sets;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.google.inject.Provides;
 
 import liquibase.resource.ClassLoaderResourceAccessor;
 
 import org.hsqldb.jdbc.JDBCDataSource;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import pl.coffeepower.guiceliquibase.annotation.GuiceLiquibase;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Set;
+import java.util.UUID;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.sql.DataSource;
 
-@Ignore
 public class Example {
 
   @SuppressWarnings("checkstyle:javadocmethod")
@@ -32,7 +42,26 @@ public class Example {
 
   @Test
   public void shouldExecuteLiquibaseMigration() throws Exception {
-    Guice.createInjector(new GuiceLiquibaseModule(), new MyLiquibaseConfigModule());
+    Injector injector = Guice.createInjector(
+        new GuiceLiquibaseModule(), new MyLiquibaseConfigModule());
+
+    Set<String> createdTables = getTablesFromDataSource(injector.getInstance(DataSource.class));
+    assertThat(createdTables, hasSize(3));
+    assertThat(createdTables,
+        containsInAnyOrder("DATABASECHANGELOG", "DATABASECHANGELOGLOCK", "EXAMPLE_TABLE"));
+  }
+
+  private Set<String> getTablesFromDataSource(DataSource dataSource) throws SQLException {
+    Set<String> createdTables = Sets.newHashSet();
+    try (Connection connection = dataSource.getConnection()) {
+      try (ResultSet tables = connection.getMetaData()
+          .getTables("PUBLIC", "PUBLIC", null, new String[] {"TABLE"})) {
+        while (tables.next()) {
+          createdTables.add(tables.getString("TABLE_NAME"));
+        }
+      }
+    }
+    return createdTables;
   }
 
   private static final class MyLiquibaseConfigModule extends AbstractModule {
@@ -45,7 +74,7 @@ public class Example {
     @Provides
     private DataSource createDataSource() {
       JDBCDataSource dataSource = new JDBCDataSource();
-      dataSource.setDatabase("jdbc:hsqldb:mem:memdb");
+      dataSource.setDatabase("jdbc:hsqldb:mem:" + UUID.randomUUID().toString());
       dataSource.setUser("SA");
       return dataSource;
     }
@@ -58,7 +87,10 @@ public class Example {
           .of(LiquibaseConfig.Builder.of(dataSource)
               .withChangeLogPath("liquibase/exampleChangeLog.xml")
               .withResourceAccessor(new ClassLoaderResourceAccessor(getClass().getClassLoader()))
-              .withDropFirst(false)
+              .withDropFirst(true)
+              .addContext("")
+              .addLabel("")
+              .addParameter("param", "value")
               .build())
           .build();
     }
