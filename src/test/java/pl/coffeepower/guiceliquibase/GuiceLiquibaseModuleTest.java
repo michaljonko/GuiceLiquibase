1506dc619bad7931889d4918ab1fbb2d0d98fd37
Ricardo Chiriboga <ricardo.chiriboga@petcircle.com.au>
2023-08-30 15:17:20 +1000

    feat: Upgrade Mockito and Hamcrest
    - Mockito to 4.x
    - Hamcrest to 2.2

--- src/test/java/pl/coffeepower/guiceliquibase/GuiceLiquibaseModuleTest.java
+++ src/test/java/pl/coffeepower/guiceliquibase/GuiceLiquibaseModuleTest.java
@@ -5,14 +5,13 @@ import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import be.joengenduvel.java.verifiers.ToStringVerifier;
@@ -142,7 +141,7 @@ public class GuiceLiquibaseModuleTest {
             }
           });
 
-      verifyZeroInteractions(dataSource);
+      verifyNoMoreInteractions(dataSource);
     } finally {
       LiquibaseConfiguration.getInstance().getConfiguration(GlobalConfiguration.class)
           .setShouldRun(true);
